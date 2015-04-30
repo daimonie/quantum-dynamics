e4452e0e037530b0800b85198bfbc4e5bb92a8ed
Daimonie <Josko@Daimonie.com>
2015-04-30 14:29:55 +0200

    Initialise numerical simulation files

---
+++ main.py
@@ -0,0 +1,4 @@
+import simulation as simulation;
+
+
+simulation();
---
+++ simulation.py
@@ -0,0 +1,7 @@
+import numpy as np;
+import scipy as sc;
+import scipy.sparse as sparse;
+
+
+def simulation():
+    print "Hello world" ;
