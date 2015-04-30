57fd8ccaae98963acb0e8c43d5a9df4ac79bbf20
Daimonie <Josko@Daimonie.com>
2015-04-30 16:00:58 +0200

    I've added in most of the functionality.
    Note that the prefactor, the parameters, aren't correct yet. So nothing can be done yet.

--- main.py
+++ main.py
@@ -1,4 +1,26 @@
-import simulation as simulation;
+from simulation import simulation   
+import scipy.constants as scc 
+import scipy.sparse as sparse  
+import numpy as np 
 
+sim = simulation() 
 
-simulation();
+sim.mass = 1.0 
+sim.chi = 1e-4  #just dx
+sim.tau = 1e-2 
+sim.hbar = 1.0  #scc.hbar
+sim.dimensions =  1000 
+
+sim.init() 
+
+xRight = 1e-9 ;
+xLeft = 0;
+
+lin = np.linspace(xLeft, xRight, sim.dimensions);
+psi = (30/xRight**5)**0.5 * lin * (xRight - lin); 
+for i in range(1,10): 
+    psi, info= sim.evolve(psi) ;
+    if info == 0:  
+        print i, psi
+    else:
+        raise Exception("For some reason, the tolerance wasn't reached.");
--- simulation.py
+++ simulation.py
@@ -1,7 +1,26 @@
-import numpy as np;
-import scipy as sc;
-import scipy.sparse as sparse;
-
-
-def simulation():
-    print "Hello world" ;
+import numpy as np 
+import scipy as sc 
+import scipy.sparse as sparse  
+import scipy.sparse.linalg as ssl 
+import scipy.constants as scc  
+class simulation:
+    
+    chi = 0.5  #dx
+    tau = 0.5  #dt
+    mass = 0.5  #mass of particle
+    hbar = 0.5 
+    
+    prefactor = 1.0 
+    
+    H = None 
+    L = None 
+    R = None 
+    def init(self):
+        self.H = -2*sparse.eye(self.dimensions, self.dimensions) +sparse.eye(self.dimensions, self.dimensions, k=1) + sparse.eye(self.dimensions, self.dimensions, k=-1) 
+        self.H.toarray() 
+        
+        self.prefactor = 1j * self.hbar * self.tau / self.mass / (self.chi)**2 
+        self.L = sparse.eye(self.dimensions,self.dimensions) + self.prefactor * self.H 
+        self.R = sparse.eye(self.dimensions,self.dimensions) - self.prefactor * self.H 
+    def evolve(self, lastPsi): 
+        return ssl.bicgstab(self.L, self.R * lastPsi, x0=lastPsi, tol=1e-10)  
