a17e7d1e4d0da8acd625c36052ebac6d0481ade8
Daimonie <josko@daimonie.com>
2015-05-06 13:18:41 +0200

    I've been trying to look at a potential, but frankly I have no idea what I'm looking at.

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import scipy.constants as scc
 import scipy.sparse as sparse  
 import numpy as np 
 
+def potential(lin, tau, r):
+    #Note: r=0 is always excluded so the trial is displayed 
+    v = 0.01e9 * lin
+    return v    
+
 sim = simulation() 
 
-sim.mass = 1.0 
-sim.chi = 1e-4  #just dx
-sim.tau = 4e-5 
-sim.hbar = 1.0  #scc.hbar
-sim.dimensions =  1000 
+sim.hbar = scc.hbar
+sim.mass = scc.m_e
 
+
+sim.dimensions =  1000
+sim.chi = 1e-9/sim.dimensions  #just dx
+#1e-18 accounts for mass and the nm, while the sim.dimensions**-2 accounts for the parameters of the simulation.
+#The last number, 0.1 at the time of writing, is the timestep in the simulation in a way that makes sense.
+sim.tau = 1e-18 * (sim.dimensions)**(-2.0) * 0.4e3
 sim.init() 
 
 xRight = 1e-9 ;
 xLeft = 0;
 
-lin = np.linspace(xLeft,xRight, sim.dimensions);
-psi = 1/(45000.0) * (30/xRight**5)**0.5 * lin * (xRight - lin); 
+lin = np.linspace(xLeft,xRight, sim.dimensions)
+psi = np.exp(-lin / (xRight - xLeft) * 10.0) * np.cos( 14e9 * lin * np.pi);
+psi /= np.max(psi);
 
 sim.setPsi(lin, psi)
-#sim.potential()
-#sim.potentialTime()
+sim.potentialCallback = potential
 sim.show();
--- simulation.py
+++ simulation.py
@@ -8,10 +8,10 @@ import matplotlib.animation as animation
 
 class simulation:
     
-    chi = 0.5  #dx
-    tau = 0.5  #dt
-    mass = 0.5  #mass of particle
-    hbar = 0.5 
+    chi     = 0.5  #dx
+    tau     = 0.5  #dt
+    mass    = 0.5  #mass of particle
+    hbar    = 0.5 
     
     prefactor = 1.0 
     
@@ -24,14 +24,18 @@ class simulation:
     ax1 = None
     ax2 = None
     ax3 = None
+    ax4 = None
     
     line1 = None
     line2 = None
     line3 = None
+    line4 = None
     
     psi = None
     
-    space = None;
+    space               = None
+    potentialCallback   = None
+    potential           = None
     def init(self):
         #Display logic
         self.initFigure()
@@ -40,13 +44,11 @@ class simulation:
         self.H = -2*sparse.eye(self.dimensions, self.dimensions) +sparse.eye(self.dimensions, self.dimensions, k=1) + sparse.eye(self.dimensions, self.dimensions, k=-1) 
         self.H.toarray() 
         
-        self.prefactor = 1j * self.hbar * self.tau / self.mass / (self.chi)**2 
-        self.L = sparse.eye(self.dimensions,self.dimensions) + self.prefactor * self.H 
-        self.R = sparse.eye(self.dimensions,self.dimensions) - self.prefactor * self.H 
+        self.prefactor = 1j * self.hbar * self.tau / 4.0 / self.mass / (self.chi)**2  
         
-        #We need to add a part here to add the time-independent part of V(r), since this is just a part of H.
-        #We also need to incorporate the logic for the time-dependent part of V(r) into the class.
-        #Both should be done by callback functions.
+        self.L = sparse.eye(self.dimensions,self.dimensions) + self.prefactor * self.H 
+        self.R = sparse.eye(self.dimensions,self.dimensions) - self.prefactor * self.H
+            
     def evolve(self): 
         self.psi, info = ssl.bicgstab(self.L, self.R * self.psi, x0=self.psi, tol=1e-10);
         #Time-dependent potential logic
@@ -60,36 +62,53 @@ class simulation:
         self.ax1.set_xlim( [self.space[0], self.space[-1]] )
         self.ax2.set_xlim( [self.space[0], self.space[-1]] )
         self.ax3.set_xlim( [self.space[0], self.space[-1]] )
+        self.ax4.set_xlim( [self.space[0], self.space[-1]] )
         
     def initFigure(self):
         self.fig = plt.figure();
         self.fig.clf()                       #clear figure
         
-        self.ax1 = self.fig.add_subplot(3,1,1)     
-        self.ax2 = self.fig.add_subplot(3,1,2)     
-        self.ax3 = self.fig.add_subplot(3,1,3)     
+        self.ax1 = self.fig.add_subplot(4,1,1)     
+        self.ax2 = self.fig.add_subplot(4,1,2)     
+        self.ax3 = self.fig.add_subplot(4,1,3)     
+        self.ax4 = self.fig.add_subplot(4,1,4)     
         
         self.ax1.grid()
         self.ax2.grid()
         self.ax3.grid()
+        self.ax4.grid()
     
         self.ax1.set_xlabel('Position')
         self.ax2.set_xlabel('Position')
         self.ax3.set_xlabel('Position')
+        self.ax4.set_xlabel('Position')
         
         self.ax1.set_ylabel('Re(Psi)')
         self.ax2.set_ylabel('Im(Psi)')
         self.ax3.set_ylabel('Norm(Psi)')
+        self.ax4.set_ylabel('Potential')
         
-        self.ax1.set_ylim( [-2, 2] );
-        self.ax2.set_ylim( [-2, 2] );
-        self.ax3.set_ylim( [-2, 2] );
+        self.ax1.set_ylim( [-1.1, 1.1] );
+        self.ax2.set_ylim( [-1.1, 1.1] );
+        self.ax3.set_ylim( [0, 1.1] );
+        self.ax4.set_ylim( [-1.1, 1.1] );
     def propagate(self, r):
         #r contains the number of the frame, starting at zero :)
         #This can be used for the time-dependent potential
+        
+        #Note that I have no idea why it has the initial psi fixed. It does look good, so I'm not going to try and fix it.
         #First, evolve
+        self.potential = self.potentialCallback( self.space, self.tau, r) 
         if r != 0:
             self.evolve();        
+            
+            exponentPotential = np.exp( -1j*self.tau * self.potential / self.hbar );
+            
+            self.psi = np.multiply(exponentPotential, self.psi)
+        
+        if r == 0:
+            self.ax4.set_ylim( [np.min(self.potential), np.max(self.potential)] );
+        
         #Now plot it 
         realPsi = np.real(self.psi);
         imagPsi = np.imag(self.psi);
@@ -98,12 +117,14 @@ class simulation:
             self.line1, = self.ax1.plot(self.space, realPsi);
             self.line2, = self.ax2.plot(self.space, imagPsi);
             self.line3, = self.ax3.plot(self.space, normPsi);
+            self.line4, = self.ax4.plot(self.space, self.potential);
         else:
             self.line1.set_ydata( realPsi )
             self.line2.set_ydata( imagPsi )
             self.line3.set_ydata( normPsi )
+            self.line4.set_ydata( self.potential );
         
-        
+        print "Animation frame %d, prefactor is %2.3e." % (r, np.absolute(self.prefactor));
     def show(self):
         
         ani = animation.FuncAnimation(self.fig, self.propagate,interval=100) 
