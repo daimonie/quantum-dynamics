4412accab7613bee9bda84b26751a4e7154c49a2
Daimonie <josko@daimonie.com>
2015-05-07 17:42:47 +0200

    Changed the dimensions times ten, length times ten, rest unchanged. Just a larger box for the same wave.
    It does respond differently, since it seems to break up before travel or something? Can it be that the constituent waves are travelling at different velocities or something?
    
    I.e. psi = sum ( c . bound state n ); speed = speed(n) ?
    
    There even seems to be a part going to the left.
    
    Surface norm is conserved.

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ sim.hbar = scc.hbar
 sim.mass = scc.m_e
 
 
-sim.dimensions =  1000
+sim.dimensions =  10000
 sim.chi = 1e-9/sim.dimensions  #just dx
 #1e-18 accounts for mass and the nm, while the sim.dimensions**-2 accounts for the parameters of the simulation.
 #The last number, 0.1 at the time of writing, is the timestep in the simulation in a way that makes sense.
@@ -22,11 +22,11 @@ sim.tau = 1e-18 * (sim.dimensions)**(-2.0) * 4 * sim.mass / sim.hbar * 1.0e3
 
 sim.init() 
 
-xRight = 1e-9 ;
+xRight = 10e-9 ;
 xLeft = 0;
 
 lin = np.linspace(xLeft,xRight, sim.dimensions)
-psi = np.exp(1j*2*np.pi*2/(1e-9)*lin) * np.exp( - (lin-0.5e-9)**2 / 2 / (0.075e-9)**2);
+psi = np.exp(-1j*2*np.pi*2/(1e-9)*lin) * np.exp( - (lin-xRight/2.0)**2 / 2.0 / (0.075e-9)**2);
 psi /= np.max(psi);
 
 sim.setPsi(lin, psi)
